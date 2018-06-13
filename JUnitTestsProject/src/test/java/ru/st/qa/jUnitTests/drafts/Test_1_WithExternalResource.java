7e6239297e10595455a32d793ede2ed97e2bcd02
DEX-6 <irina14.91@bk.ru>
2018-06-13 16:57:23 +0400

    Тестовый тест с использованием @Rule вместо @Before И @After

---
+++ JUnitTestsProject/src/test/java/ru/st/qa/jUnitTests/drafts/Test_1_WithExternalResource.java
@@ -0,0 +1,35 @@
+package ru.st.qa.jUnitTests.drafts;
+
+import org.junit.*;
+import org.junit.rules.ExternalResource;
+import org.junit.runners.MethodSorters;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class Test_1_WithExternalResource {
+
+    private String className = Test_1_WithExternalResource.class.getSimpleName();
+
+    @Rule
+    public final ExternalResource externalResource = new ExternalResource() {
+        @Override
+        protected void before() throws Throwable {
+            System.out.println("-------------------------------");
+            System.out.println(className + " test1 setUp");
+        }
+
+        @Override
+        protected void after() {
+            System.out.println(className + " test1 tear down");
+        }
+    };
+
+    @Test
+    public void bTestMethod1() {
+        System.out.println(className + " test method 1");
+    }
+
+    @Test
+    public void aTestMethod2() {
+        System.out.println(className + " test method 2");
+    }
+}
